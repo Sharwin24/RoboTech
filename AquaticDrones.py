ea6f64379bf8d6f9b07cfe16847814c5672f0b1f
Sharwin Patil <sharwin24@gmail.com>
2022-04-02 17:50:31 -0400

    Removed comments and added pathFind

--- AquaticDrones.py
+++ AquaticDrones.py
@@ -3,48 +3,6 @@ from typing import List, Tuple
 from graphics import Image
 
 
-# Going to change to an image that we've created
-# class AquaticSupervisorImage(_BBox):
-
-#     def __init__(self, p1, p2):
-#         _BBox.__init__(self, p1, p2)
-
-#     def __repr__(self):
-#         return "AquaticSupervisor({}, {})".format(str(self.p1), str(self.p2))
-
-#     def _draw(self, canvas, options):
-#         p1 = self.p1
-#         p2 = self.p2
-#         x1, y1 = canvas.toScreen(p1.x, p1.y)
-#         x2, y2 = canvas.toScreen(p2.x, p2.y)
-#         return canvas.create_rectangle(x1, y1, x2, y2, options)
-
-#     def clone(self):
-#         other = Rectangle(self.p1, self.p2)
-#         other.config = self.config.copy()
-#         return other
-
-
-# class AquaticDroneImage(_BBox):
-#     def __init__(self, p1, p2):
-#         _BBox.__init__(self, p1, p2)
-
-#     def __repr__(self):
-#         return "AquaticDrone({}, {})".format(str(self.p1), str(self.p2))
-
-#     def clone(self):
-#         other = Oval(self.p1, self.p2)
-#         other.config = self.config.copy()
-#         return other
-
-#     def _draw(self, canvas, options):
-#         p1 = self.p1
-#         p2 = self.p2
-#         x1, y1 = canvas.toScreen(p1.x, p1.y)
-#         x2, y2 = canvas.toScreen(p2.x, p2.y)
-#         return canvas.create_oval(x1, y1, x2, y2, options)
-
-
 @dataclass
 class AquaticDrone():
     """Class representation for an AquaticDrone Robot. The robot understands its pose: Position and Orientation in order to calculate future trajectory"""
@@ -67,7 +25,6 @@ class AquaticDrone():
 
     def withinRangeOfSupervisor(self, range: float) -> bool:
         return abs(self.getSupervisorPos - self.position) <= range
-
 
 
 @dataclass
