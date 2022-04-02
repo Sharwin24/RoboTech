058528b2f3682045245f571b6d0273976c817b25
Sharwin Patil <sharwin24@gmail.com>
2022-04-02 12:31:56 -0400

    Added graphics attributes

--- AquaticDrones/AquaticDrone.py
+++ AquaticDrones/AquaticDrone.py
@@ -1,36 +1,72 @@
 from dataclasses import dataclass
-from pose import *
-from typing import List
+from typing import List, Tuple
 from graphics import *
-import numpy as np
+from graphics import _BBox
 
 
-@dataclass
-class AquaticSuperVisorImage(GraphicsObject):
-    def __init__(self, x, y):
-        GraphicsObject.__init__(self, ["outline", "fill"])
-        self.setFill = self.setOutline
-        self.x = float(x)
-        self.y = float(y)
+class AquaticSupervisorImage(_BBox):
+
+    def __init__(self, p1, p2):
+        _BBox.__init__(self, p1, p2)
+
+    def __repr__(self):
+        return "AquaticSupervisor({}, {})".format(str(self.p1), str(self.p2))
+
+    def _draw(self, canvas, options):
+        p1 = self.p1
+        p2 = self.p2
+        x1, y1 = canvas.toScreen(p1.x, p1.y)
+        x2, y2 = canvas.toScreen(p2.x, p2.y)
+        return canvas.create_rectangle(x1, y1, x2, y2, options)
+
+    def clone(self):
+        other = Rectangle(self.p1, self.p2)
+        other.config = self.config.copy()
+        return other
+
+
+class AquaticDroneImage(_BBox):
+    def __init__(self, p1, p2):
+        _BBox.__init__(self, p1, p2)
+
+    def __repr__(self):
+        return "AquaticDrone({}, {})".format(str(self.p1), str(self.p2))
+
+    def clone(self):
+        other = Oval(self.p1, self.p2)
+        other.config = self.config.copy()
+        return other
+
+    def _draw(self, canvas, options):
+        p1 = self.p1
+        p2 = self.p2
+        x1, y1 = canvas.toScreen(p1.x, p1.y)
+        x2, y2 = canvas.toScreen(p2.x, p2.y)
+        return canvas.create_oval(x1, y1, x2, y2, options)
 
 
 @dataclass
 class AquaticSupervisor():
     """Class representation for the supervisor robot that deploys drones with their own subroutines"""
-    position: Position
+    position: Tuple[int, int]
     dronesList: List[AquaticDrone]
+    image: AquaticSupervisorImage
 
 
 @dataclass
 class AquaticDrone():
     """Class representation for an AquaticDrone Robot. The robot understands its pose: Position and Orientation in order to calculate future trajectory"""
-    position: Position
-    vector: MovementVector
+    position: Tuple[int, int]
+    vector: Tuple[float, float]
     supervisor: AquaticSupervisor
+    image: AquaticDroneImage
 
-    def getSupervisorPos(self) -> Position:
+    def getSupervisorPos(self) -> Tuple[int, int]:
         return self.supervisor.position
+
+    def withinRangeOfSupervisor(self, range: float):
+        return abs(self.getSupervisorPos - self.position) <= range
 
 
 if __name__ == "__main__":
-    print("Ran")
+    print("Hello")
