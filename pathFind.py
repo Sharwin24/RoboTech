9c566b54dcf9c5affdbb38610c59c6baf5c2f363
Sharwin Patil <sharwin24@gmail.com>
2022-04-02 19:13:02 -0400

    Created utility and added navgrid class

--- pathFind.py
+++ pathFind.py
@@ -2,12 +2,17 @@ from dataclasses import dataclass
 from typing import List
 from typing import Tuple
 from typing import Set
+from utility import *
 
 
 @dataclass
-class Position():
-    x: int
-    y: int
+class NavigationGrid():
+    grid: List[List[Node]]
+    width: int
+    height: int
+
+    def inBounds(p: Position) -> bool:
+        pass
 
 
 @dataclass
@@ -20,10 +25,13 @@ class Node():
         self.hasAlgae = False
 
     def __eq__(self, __o: object) -> bool:
-        pass
+        if isinstance(__o, Node):
+            return self.position == __o.position and self.hasAlgae == __o.hasAlgae and self.isWall == __o.isWall
+        else:
+            return False
 
     def __hash__(self) -> int:
-        pass
+        return hash(self.position) + hash(self.hasAlgae) + hash(self.isWall)
 
 
 @dataclass
@@ -31,23 +39,41 @@ class StateSpace():
     dronePosition: Position
     path: List[Position]
     mustVisitNodes: Set[Position]
-    navigationGrid: List[List[Node]]  # 2D Array of Position
+    navigationGrid: NavigationGrid  # 2D Array of Position
 
-    def getFCost() -> int:
-        return 0
+    def getFCost(self) -> float:
+        return self.getGCost() + self.getHCost()
+
+    def getHCost(self) -> float:
+        return 0  # No Heuristic yet
+
+    def getGCost(self) -> float:
+        return len(self.path)
 
     def isGoalState(self) -> bool:
         return len(self.mustVisitNodes) == 0
 
-    def __eq__(self, __o: object) -> bool:
+    def getSuccessors(self) -> List[StateSpace]:
         pass
 
+    def getPath(self) -> List[Position]:
+        return self.path
+
+    def getMVNodes(self) -> Set[Position]:
+        return self.mustVisitNodes
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, StateSpace):
+            return self.mustVisitNodes == __o.mustVisitNodes and self.dronePosition == __o.dronePosition
+        else:
+            return False
+
     def __hash__(self) -> int:
-        pass
+        return hash(self.dronePosition) + hash(self.mustVisitNodes)
 
 
 @dataclass
 class PathFind():
-
     def shortestPath(self, startingState: StateSpace) -> List[Position]:
+        '''Finds the shortest path from a starting state using A* search'''
         pass
---
+++ utility.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from typing import List
+from typing import Tuple
+from typing import Set
+
+from pathFind import NavigationGrid, StateSpace, Node
+
+
+@dataclass
+class Position():
+    x: int
+    y: int
+
+    def __hash__(self) -> int:
+        return hash(self.x) + hash(self.y)
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Position):
+            return self.x == __o.x and self.y == __o.y
+        else:
+            return False
+
+
+def getNeighbors(p: Position, navGrid: NavigationGrid) -> List[StateSpace]:
+    up = Position(p.x, p.y + 1)
+    down = Position(p.x, p.y - 1)
+    left = Position(p.x - 1, p.y)
+    right = Position(p.x + 1, p.y)
+    upRight = Position(p.x + 1, p.y + 1)
+    upLeft = Position(p.x - 1, p.y + 1)
+    downRight = Position(p.x + 1, p.y - 1)
+    downLeft = Position(p.x - 1, p.y - 1)
+    possiblePositions = [up, down, left, right,
+                         upRight, upLeft, downRight, downLeft]
+    neighbors = []
+    for p in possiblePositions:
+        pass
+    return neighbors
+
+
+def getManhattanDistance(p1: Position, p2: Position) -> int:
+    return abs(p2.x - p1.x) + abs(p2.y - p1.y)
+
+
+def getEuclideanDistance(p1: Position, p2: Position) -> float:
+    return pow(p2.x-p1.x, 2) + pow(p2.y-p1.y, 2)
